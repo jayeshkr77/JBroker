f17848d6839b69fdfc974315dac5b75ac2ae7f24
jayeshkr77 <cooljayeshkr7@gmail.com>
2022-02-06 22:38:58 +0530

    Added login route

--- routes/user.js
+++ routes/user.js
@@ -8,6 +8,8 @@ const authentication = require('../middlewares/authentication');
 // /api/user
 router.get('/verify/:jwt',userController.verify);
 router.post('/register/',userController.register);
+router.post('/login/',userController.login);
 router.post('/updatepin',authentication,userController.updatePin);
+router.post('/setpin/', userController.setPin);
 
 module.exports = router;
